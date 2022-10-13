694df80ce243a70de178f9c2390ff59b2ea6d941
PamperedGenius6098 <56474204+PamperedGenius6098@users.noreply.github.com>
2022-10-13 19:35:46 +0530

    Update check-for-prime.py

--- check-for-prime.py
+++ check-for-prime.py
@@ -1,7 +1,7 @@
 #check for prime
 ch='y'
 while (ch=='y') or (ch=='Y'):
-    a=int(input("enter no. you want to check:"))
+    a=int(input("Enter no. you want to check:"))
     b=int(a/2)+1
     #print("b =",b)
     for i in range(2,b+1):
@@ -10,9 +10,9 @@ while (ch=='y') or (ch=='Y'):
         if c==0:
             break
     if c!=0:
-        print("entered no. is a prime no.")
+        print("Entered no. is a prime no.")
     else:
-        print("entered no. is not a prime no.")
-    ch=input("do you want to check more numbers y/n:")
+        print("Entered no. is not a prime no.")
+    ch=input("Do you want to check more numbers y/n:")
 print("Good bye")
 print("Hope i helped!!")
